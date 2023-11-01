446b5112640ceed73773d617654df2912b971804
Murad Mammadli <mammamur@fit.cvut.cz>
2023-11-01 14:20:13 +0100

    First commit

---
+++ src/test/java/com/example/thesisproject/ThesisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thesisproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
