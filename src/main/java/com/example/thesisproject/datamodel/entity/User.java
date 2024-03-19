9974e8800e6ac042661008da3a2909721f94a508
Murad Mammadli <mammamur@fit.cvut.cz>
2024-03-19 23:25:22 +0100

    Update User.java

--- src/main/java/com/example/thesisproject/datamodel/entity/User.java
+++ src/main/java/com/example/thesisproject/datamodel/entity/User.java
@@ -31,10 +31,6 @@ public  class User {
     private String password;
 
 
-//    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-//    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-//            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id" ))
-//    private List<Role> roles = new ArrayList<>();
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
